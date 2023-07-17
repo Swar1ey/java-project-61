b6769d682ba76081fd2b6d307e51cc6f8f10d37e
Timur <kta.com@bk.ru>
2023-07-17 21:02:10 +0300

    add Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,2 +1,30 @@
-package hexlet.code;public class Engine {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    private static final int MAX_ROUNDS = 3;
+    public static void playGame(String desGame, GameLogic gameLogic) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Welcome to the Brain Games!\n"
+                +
+                "May I have your name? ");
+        String name = scanner.next();
+        System.out.println("Hello, " + name + "!\n"
+                +
+                "What is the result of the expression?");
+
+        for (var i = 0; i != MAX_ROUNDS; i++) {
+            if (!gameLogic.playRound()) {
+                System.out.println("Let's try again, " + name + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + name + "!");
+    }
+
+    public interface GameLogic {
+        boolean playRound();
+    }
 }
