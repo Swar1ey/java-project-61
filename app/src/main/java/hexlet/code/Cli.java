7e51429ffefe32c59199b5af8a2d16cb454e4eba
Timur <kta.com@bk.ru>
2023-07-17 13:01:43 +0300

    add Cli.java and update App.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,17 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
+
+public class Cli {
+    public static void yourName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String name = scanner.next();
+        System.out.println("Hello, " + name + "!");
+
+        scanner.close();
+    }
 }
