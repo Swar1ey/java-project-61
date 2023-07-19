75c871fe684d41017167238f394ebef58bdf50bd
Timur <kta.com@bk.ru>
2023-07-19 17:20:11 +0300

    update GameGCD.java

--- app/src/main/java/hexlet/code/game/GameGCD.java
+++ app/src/main/java/hexlet/code/game/GameGCD.java
@@ -4,29 +4,43 @@ import hexlet.code.Engine;
 
 import java.util.Scanner;
 import java.util.Random;
-public class GameGCD implements Engine.GameLogic {
-    public boolean playRound() {
-        Scanner scanner = new Scanner(System.in);
+public class GameGCD {
+    private static final String RULES = "Find the greatest common divisor of given numbers.";
+    private static final int MAX_NUMBER = 100;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_ROUNDS = 3;
+    private static int count = 0;
+    static String question;
+    public static void game() {
+        Engine.greeting(RULES);
+
+        while (count < MAX_ROUNDS) {
+            Scanner scanner = new Scanner(System.in);
+            int result = getResult();
+            System.out.println(question);
+            System.out.print("Your answer: ");
+            int answer = scanner.nextInt();
+            if (Engine.isCorrectAnswer(result == answer, result, answer)) {
+                break;
+            } else {
+                count++;
+            }
+            Engine.congratulations(count == MAX_ROUNDS);
+        }
+    }
+    public static int getResult() {
         Random random = new Random();
-        int temp = 0;
+        int temp;
 
-        int number1 = random.nextInt(100) + 1;
-        int number2 = random.nextInt(100) + 1;
+        int number1 = random.nextInt(MIN_NUMBER, MAX_NUMBER);
+        int number2 = random.nextInt(MIN_NUMBER, MAX_NUMBER);
+        question = ("Question: " + number1 + ' ' + number2);
 
-        System.out.println("Question: " + number1 + ' ' + number2);
-        System.out.print(("Your answer: "));
-        int answer = scanner.nextInt();
         while (number2 != 0) {
             temp = number1 % number2;
             number1 = number2;
             number2 = temp;
         }
-        if (answer == number1) {
-            System.out.println("Correct!");
-            return true;
-        } else {
-            System.out.println(answer + " is wrong answer ;(. Correct answer was " + number1 + ".");
-            return false;
-        }
+        return number1;
     }
 }
