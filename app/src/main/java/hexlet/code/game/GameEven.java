e3e1f7a575e091e06770a5ded70e16f8f5b009aa
Timur <kta.com@bk.ru>
2023-07-19 17:19:41 +0300

    update GameEven.java

--- app/src/main/java/hexlet/code/game/GameEven.java
+++ app/src/main/java/hexlet/code/game/GameEven.java
@@ -4,26 +4,31 @@ import hexlet.code.Engine;
 
 import java.util.Scanner;
 import java.util.Random;
-public class GameEven implements Engine.GameLogic {
-    public boolean playRound() {
+public class GameEven {
+    private static final String RULES = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    private static final int MAX_NUMBER = 100;
+    private static final int MAX_ROUNDS = 3;
+    private static int count = 0;
+    public static void game() {
+        Engine.greeting(RULES);
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        while (count < MAX_ROUNDS) {
+            Scanner scanner = new Scanner(System.in);
+            Random random = new Random();
+            int number = random.nextInt(MAX_NUMBER);
 
-        int number = random.nextInt(100);
-        System.out.println("Question: " + number);
-        System.out.print(("Your answer: "));
-        String answer = scanner.next();
-        boolean isEven = number % 2 == 0;
-        boolean correctAnswer = ((!isEven && answer.equals("no")) || (isEven && answer.equals("yes")));
-        String isCorrectAnswer = isEven ? "'yes'" : "'no'";
-
-        if (correctAnswer) {
-            System.out.println("Correct!");
-            return true;
-        } else {
-            System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was " + isCorrectAnswer + ".");
-            return false;
+            System.out.println("Question: " + number);
+            System.out.print(("Your answer: "));
+            String answer = scanner.next();
+            boolean isEven = number % 2 == 0;
+            boolean correctAnswer = ((!isEven && answer.equals("no")) || (isEven && answer.equals("yes")));
+            String isCorrectAnswer = isEven ? "'yes'" : "'no'";
+            if (Engine.isCorrectAnswer(correctAnswer, isCorrectAnswer, answer)) {
+                break;
+            } else {
+                count++;
+            }
+            Engine.congratulations(count == MAX_ROUNDS);
         }
     }
 }
