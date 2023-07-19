0653ebbab52c5ed1cf52c438ebd1a6ff9154acb3
Timur <kta.com@bk.ru>
2023-07-19 17:20:52 +0300

    update GameProgression.java

--- app/src/main/java/hexlet/code/game/GameProgression.java
+++ app/src/main/java/hexlet/code/game/GameProgression.java
@@ -4,36 +4,49 @@ import hexlet.code.Engine;
 
 import java.util.Scanner;
 import java.util.Random;
-public class GameProgression implements Engine.GameLogic {
-    public boolean playRound() {
-        Scanner scanner = new Scanner(System.in);
+public class GameProgression {
+    private static final String RULES = "What number is missing in the progression?";
+    private static final int MAX_NUMBER = 100;
+    private static final int MAX_INDEX = 10;
+    private static final int MAX_ROUNDS = 3;
+    private static final int MIN_PROGRESSION = 1;
+    private static final int MAX_PROGRESSION = 15;
+    private static int count = 0;
+    static String question;
+    public static void game() {
+        Engine.greeting(RULES);
+
+        while (count < MAX_ROUNDS) {
+            Scanner scanner = new Scanner(System.in);
+            int result = getResult();
+            System.out.println(question);
+            System.out.print("Your answer: ");
+            int answer = scanner.nextInt();
+            if (Engine.isCorrectAnswer(result == answer, result, answer)) {
+                break;
+            } else {
+                count++;
+            }
+            Engine.congratulations(count == MAX_ROUNDS);
+        }
+    }
+    public static int getResult() {
         Random random = new Random();
-        int hideIndex = random.nextInt(10);
-        int startProgrs = random.nextInt(100);
-        int increase = random.nextInt(1, 15);
-        var result = 0;
-        System.out.print("Question: ");
-        for (var i = 0; i < 10; i++) {
+        int hideIndex = random.nextInt(MAX_INDEX);
+        int startProgrs = random.nextInt(MAX_NUMBER);
+        int increase = random.nextInt(MIN_PROGRESSION, MAX_PROGRESSION);
+        int result = 0;
+        question = "Question: ";
+        for (var i = 0; i < MAX_INDEX; i++) {
             if (hideIndex != i) {
-                System.out.print(startProgrs + " ");
+                question = question + (startProgrs + " ");
                 startProgrs += increase;
             } else {
-                System.out.print(".. ");
+                question = question + (".. ");
                 result = startProgrs;
                 startProgrs += increase;
             }
-            if (i == 9) {
-                System.out.println();
-            }
         }
-        System.out.print("Your answer: ");
-        int answer = scanner.nextInt();
-        if (answer == result) {
-            System.out.println("Correct!");
-            return true;
-        } else {
-            System.out.println(answer + " is wrong answer ;(. Correct answer was " + result + ".");
-            return false;
-        }
+        return result;
     }
 }
