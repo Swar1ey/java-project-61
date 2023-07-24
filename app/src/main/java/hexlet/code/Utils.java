6f89ca4f2f7cefcdb2495f2475ecd14f1068857b
Timur <kta.com@bk.ru>
2023-07-24 16:42:04 +0300

    add Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,2 +1,11 @@
-package hexlet.code;public class Utils {
+package hexlet.code;
+
+public class Utils {
+    public static final int DEFAULT_MIN_NUMBER = 0;
+    public static final int DEFAULT_MAX_NUMBER = 100;
+
+    public static int generateRandomNumber(int lowerBorder, int upperBorder) {
+        return (int) (Math.random() * (upperBorder - lowerBorder + 1) + lowerBorder);
+    }
 }
+
