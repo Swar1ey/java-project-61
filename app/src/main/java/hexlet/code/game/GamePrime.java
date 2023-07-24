91415dc92f61bd71da06a41f716e77ee36f043a2
Timur <kta.com@bk.ru>
2023-07-24 16:44:40 +0300

    update GamePrime.java

--- app/src/main/java/hexlet/code/game/GamePrime.java
+++ app/src/main/java/hexlet/code/game/GamePrime.java
@@ -1,44 +1,29 @@
 package hexlet.code.game;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
-import java.util.Scanner;
+import hexlet.code.Utils;
 
 public class GamePrime {
     private static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-    private static final int MAX_NUMBER = 100;
     private static final int MIN_NUMBER = 1;
-    private static final int MAX_ROUNDS = 3;
-    private static int count = 0;
-    private static String question;
     public static void game() {
-        Engine.greeting(RULES);
-
-        while (count < MAX_ROUNDS) {
-            Scanner scanner = new Scanner(System.in);
-            boolean result = isPrime();
-            System.out.println(question);
-            System.out.print(("Your answer: "));
-            String answer = scanner.next();
-            String isCorrectAnswer = result ? "yes" : "no";
-            if (Engine.isCorrectAnswer(isCorrectAnswer.equals(answer), isCorrectAnswer, answer)) {
-                break;
-            } else {
-                count++;
-            }
-            Engine.congratulations(isCorrectAnswer.equals(answer));
+        String[][] questionAnswer = new String[Engine.MAX_ROUNDS][];
+        for (var i = 0; i < Engine.MAX_ROUNDS; i++) {
+            questionAnswer[i] = getResult();
         }
+        Engine.playGame(questionAnswer, RULES);
     }
-    public static boolean isPrime() {
-        Random random = new Random();
-        int number = random.nextInt(MIN_NUMBER, MAX_NUMBER);
-        question = ("Question: " + number);
+    public static String[] getResult() {
+        String[] questionAnswers = new String[2];
+        int number = Utils.generateRandomNumber(MIN_NUMBER, Utils.DEFAULT_MAX_NUMBER);
+        questionAnswers[0] = String.valueOf(number);
         for (var i = 2; i < (number + 1) / 2; i++) {
             if (number % i == 0) {
-                return false;
+                questionAnswers[1] = "no";
+                return questionAnswers;
             }
         }
-        return true;
+        questionAnswers[1] = "yes";
+        return questionAnswers;
     }
 }
