4a40f4d7b1e6b73ed87c3f50bbe7c3424d631198
Timur <kta.com@bk.ru>
2023-07-24 16:43:47 +0300

    update GameGCD.java

--- app/src/main/java/hexlet/code/game/GameGCD.java
+++ app/src/main/java/hexlet/code/game/GameGCD.java
@@ -1,46 +1,31 @@
 package hexlet.code.game;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
-import java.util.Scanner;
-import java.util.Random;
 public class GameGCD {
     private static final String RULES = "Find the greatest common divisor of given numbers.";
-    private static final int MAX_NUMBER = 100;
     private static final int MIN_NUMBER = 1;
-    private static final int MAX_ROUNDS = 3;
-    private static int count = 0;
-    private static String question;
     public static void game() {
-        Engine.greeting(RULES);
-
-        while (count < MAX_ROUNDS) {
-            Scanner scanner = new Scanner(System.in);
-            int result = getResult();
-            System.out.println(question);
-            System.out.print("Your answer: ");
-            int answer = scanner.nextInt();
-            if (Engine.isCorrectAnswer(result == answer, result, answer)) {
-                break;
-            } else {
-                count++;
-            }
-            Engine.congratulations(count == MAX_ROUNDS);
+        String[][] questionAnswer = new String[Engine.MAX_ROUNDS][];
+        for (var i = 0; i < Engine.MAX_ROUNDS; i++) {
+            questionAnswer[i] = getResult();
         }
+        Engine.playGame(questionAnswer, RULES);
     }
-    public static int getResult() {
-        Random random = new Random();
+    public static String[] getResult() {
+        String[] questionAnswers = new String[2];
         int temp;
-
-        int number1 = random.nextInt(MIN_NUMBER, MAX_NUMBER);
-        int number2 = random.nextInt(MIN_NUMBER, MAX_NUMBER);
-        question = ("Question: " + number1 + ' ' + number2);
+        int number1 = Utils.generateRandomNumber(MIN_NUMBER, Utils.DEFAULT_MAX_NUMBER);
+        int number2 = Utils.generateRandomNumber(MIN_NUMBER, Utils.DEFAULT_MAX_NUMBER);
 
         while (number2 != 0) {
             temp = number1 % number2;
             number1 = number2;
             number2 = temp;
         }
-        return number1;
+        questionAnswers[0] = number1 + " " + number2;
+        questionAnswers[1] = String.valueOf(number1);
+        return questionAnswers;
     }
 }
