634b5759a08052c799c47e349d7a93072d4194aa
Timur <kta.com@bk.ru>
2023-07-26 17:16:30 +0300

    update GameGCD.java

--- app/src/main/java/hexlet/code/game/GameGCD.java
+++ app/src/main/java/hexlet/code/game/GameGCD.java
@@ -6,6 +6,7 @@ import hexlet.code.Utils;
 public class GameGCD {
     private static final String RULES = "Find the greatest common divisor of given numbers.";
     private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 100;
     public static void game() {
         String[][] questionAnswer = new String[Engine.MAX_ROUNDS][];
         for (var i = 0; i < Engine.MAX_ROUNDS; i++) {
@@ -15,17 +16,24 @@ public class GameGCD {
     }
     public static String[] getResult() {
         String[] questionAnswers = new String[2];
+        int number1 = Utils.generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
+        int number2 = Utils.generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
+        String result = isGCD(number1, number2);
+
+        questionAnswers[0] = number1 + " " + number2;
+        questionAnswers[1] = result;
+
+        return questionAnswers;
+    }
+    public static String isGCD(int number1, int number2) {
         int temp;
-        int number1 = Utils.generateRandomNumber(MIN_NUMBER, Utils.DEFAULT_MAX_NUMBER);
-        int number2 = Utils.generateRandomNumber(MIN_NUMBER, Utils.DEFAULT_MAX_NUMBER);
 
         while (number2 != 0) {
             temp = number1 % number2;
             number1 = number2;
             number2 = temp;
         }
-        questionAnswers[0] = number1 + " " + number2;
-        questionAnswers[1] = String.valueOf(number1);
-        return questionAnswers;
+
+        return String.valueOf(number1);
     }
 }
