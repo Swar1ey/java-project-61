9831ee8b7ff1413aab30755cf25a40bafd9d7392
Timur <kta.com@bk.ru>
2023-07-26 17:15:51 +0300

    update GameCalc.java

--- app/src/main/java/hexlet/code/game/GameCalc.java
+++ app/src/main/java/hexlet/code/game/GameCalc.java
@@ -6,6 +6,9 @@ import hexlet.code.Utils;
 public class GameCalc {
     private static final String RULES = "What is the result of the expression?";
     private static final int COUNT_OPERTOR = 2;
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 100;
+    private static final String[] CALCULATOR_OPERATORS = {"+", "-", "*"};
 
     public static void game() {
         String[][] questionAnswer = new String[Engine.MAX_ROUNDS][];
@@ -17,23 +20,27 @@ public class GameCalc {
     public static String[] getResult() {
         String[] questionAnswers = new String[2];
 
-        int number1 = Utils.generateRandomNumber(Utils.DEFAULT_MIN_NUMBER, Utils.DEFAULT_MAX_NUMBER);
-        int number2 = Utils.generateRandomNumber(Utils.DEFAULT_MIN_NUMBER, Utils.DEFAULT_MAX_NUMBER);
-        int operator = Utils.generateRandomNumber(Utils.DEFAULT_MIN_NUMBER, COUNT_OPERTOR);
+        int number1 = Utils.generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
+        int number2 = Utils.generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
+        int operator = Utils.generateRandomNumber(MIN_NUMBER, COUNT_OPERTOR);
+        String chooseOperator = CALCULATOR_OPERATORS[operator];
 
-        switch (operator) {
-            case 0:
-                questionAnswers[0] = (number1 + " + " + number2);
-                questionAnswers[1] = String.valueOf(number1 + number2);
-                return questionAnswers;
-            case 1:
-                questionAnswers[0] = (number1 + " - " + number2);
-                questionAnswers[1] = String.valueOf(number1 - number2);
-                return questionAnswers;
+        questionAnswers[0] = number1 + chooseOperator + number2;
+        questionAnswers[1] = String.valueOf(Calculator(number1, number2, chooseOperator));
+
+        return questionAnswers;
+    }
+
+    public static int Calculator(int number1, int number2, String chooseOperator) {
+        switch (chooseOperator) {
+            case "+":
+                return number1 + number2;
+            case "-":
+                return number1 - number2;
+            case "*":
+                return number1 * number2;
             default:
-                questionAnswers[0] = (number1 + " * " + number2);
-                questionAnswers[1] = String.valueOf(number1 * number2);
-                return questionAnswers;
+                throw new NullPointerException();
         }
     }
 }
