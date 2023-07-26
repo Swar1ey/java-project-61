9b799af4a62f4ed07034ef9902431fb3abc0261b
Timur <kta.com@bk.ru>
2023-07-26 17:16:50 +0300

    update GamePrime.java

--- app/src/main/java/hexlet/code/game/GamePrime.java
+++ app/src/main/java/hexlet/code/game/GamePrime.java
@@ -6,6 +6,7 @@ import hexlet.code.Utils;
 public class GamePrime {
     private static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
     private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 100;
     public static void game() {
         String[][] questionAnswer = new String[Engine.MAX_ROUNDS][];
         for (var i = 0; i < Engine.MAX_ROUNDS; i++) {
@@ -13,17 +14,21 @@ public class GamePrime {
         }
         Engine.playGame(questionAnswer, RULES);
     }
+
     public static String[] getResult() {
         String[] questionAnswers = new String[2];
-        int number = Utils.generateRandomNumber(MIN_NUMBER, Utils.DEFAULT_MAX_NUMBER);
+        int number = Utils.generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
         questionAnswers[0] = String.valueOf(number);
+        questionAnswers[1] = isPrime(number);
+        return questionAnswers;
+    }
+
+    public static String isPrime(int number) {
         for (var i = 2; i < (number + 1) / 2; i++) {
             if (number % i == 0) {
-                questionAnswers[1] = "no";
-                return questionAnswers;
+                return "no";
             }
         }
-        questionAnswers[1] = "yes";
-        return questionAnswers;
+        return "yes";
     }
 }
