8ca8cb9fba23854e1a191c4a512cee34796a64b2
Timur <kta.com@bk.ru>
2023-07-26 17:16:11 +0300

    update GameEven.java

--- app/src/main/java/hexlet/code/game/GameEven.java
+++ app/src/main/java/hexlet/code/game/GameEven.java
@@ -5,6 +5,8 @@ import hexlet.code.Utils;
 
 public class GameEven {
     private static final String RULES = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 100;
     public static void game() {
         String[][] questionAnswer = new String[Engine.MAX_ROUNDS][];
         for (var i = 0; i < Engine.MAX_ROUNDS; i++) {
@@ -14,10 +16,12 @@ public class GameEven {
     }
     public static String[] getResult() {
         String[] questionAnswers = new String[2];
-        int number = Utils.generateRandomNumber(Utils.DEFAULT_MIN_NUMBER, Utils.DEFAULT_MAX_NUMBER);
+        int number = Utils.generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
         questionAnswers[0] = String.valueOf(number);
-        boolean isEven = number % 2 == 0;
-        questionAnswers[1] = isEven ? "yes" : "no";
+        questionAnswers[1] = isEven(number) ? "yes" : "no";
         return questionAnswers;
+    }
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
