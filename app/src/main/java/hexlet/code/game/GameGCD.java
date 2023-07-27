e6aed33c9c19e3978bb42a0da53f5dc5a1602331
Timur <kta.com@bk.ru>
2023-07-27 13:01:19 +0300

    update GameGCD.java

--- app/src/main/java/hexlet/code/game/GameGCD.java
+++ app/src/main/java/hexlet/code/game/GameGCD.java
@@ -7,6 +7,7 @@ public class GameGCD {
     private static final String RULES = "Find the greatest common divisor of given numbers.";
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 100;
+
     public static void game() {
         String[][] questionAnswer = new String[Engine.MAX_ROUNDS][];
         for (var i = 0; i < Engine.MAX_ROUNDS; i++) {
@@ -14,6 +15,7 @@ public class GameGCD {
         }
         Engine.playGame(questionAnswer, RULES);
     }
+
     public static String[] getResult() {
         String[] questionAnswers = new String[2];
         int number1 = Utils.generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
@@ -25,6 +27,7 @@ public class GameGCD {
 
         return questionAnswers;
     }
+
     public static String isGCD(int number1, int number2) {
         int temp;
 
